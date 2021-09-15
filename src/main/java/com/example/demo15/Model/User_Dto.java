0e440cc58535cc3aaed4353649b27f252a49a6d1
sabya <sabyasacheebeherappl@gmail.com>
2021-09-15 19:07:46 +0530

    Dto Change

--- src/main/java/com/example/demo15/Model/User_Dto.java
+++ src/main/java/com/example/demo15/Model/User_Dto.java
@@ -7,27 +7,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class User_Dto {
 
     @JsonIgnore  //IF value is to be ignored that is not to be shown in the response
-    private int id=0;
+    private int id = 0;
     @JsonProperty(value = "name")
-    public  String name=null;
-    public  String mail;
+    public String name = null;
     @JsonIgnore
-    public  int age;
+    public int age;
+    @JsonProperty(value = "salary")
+    public int sal;
 
-    public String getMail() {
-        return mail;
+    public int getSal() {
+        return sal;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setSal(int sal) {
+        this.sal = sal;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    public User_Dto ()
+    {
+        this.name=name;
+        this.id=id;
+        this.age=age;
     }
 
     public int getId() {
@@ -44,5 +44,13 @@ public class User_Dto {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- src/main/java/com/example/demo15/Service/ServiceConverter.java
+++ src/main/java/com/example/demo15/Service/ServiceConverter.java
@@ -1,24 +1,43 @@
-//package com.example.demo15.Service;
-//
-//import com.example.demo15.Model.User;
-//import com.example.demo15.Model.User_Dto;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ServiceConverter {
-//
-//    @Autowired
-//    private ModelMapper modelMapper;
-//
-//    public User_Dto convertToDto(User userObject)
-//    {
-//        return modelMapper.map(userObject, User_Dto.class);
-//    }
-//
-//    /*
-//     * public UserDto convertToDto(List<User> userListDatas) { return
-//     * modelMapper.map(userListDatas, UserDto.class); }
-//     */
-//}
+package com.example.demo15.Service;
+
+import com.example.demo15.Model.User;
+import com.example.demo15.Model.User_Dto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ServiceConverter
+{
+    public User_Dto entityToDto(User user)
+    {
+        User_Dto dto=new User_Dto();
+        dto.setId(user.getId());
+        dto.setAge(user.getAge());
+        dto.setName(user.getName());
+        dto.setSal(32000);
+
+        return dto;
+    }
+    public List<User_Dto> entityToDto(List<User>userList)
+    {
+        return userList.stream().map(x->entityToDto(x)).collect(Collectors.toList());
+    }
+    public User DtoToEntity(User_Dto dtos)
+    {
+        User users=new User();
+        users.setAge(dtos.getAge());
+        users.setId(dtos.getId());
+        users.setName(dtos.getName());
+        users.setMail("sabyasachi@gmail.com");
+
+        return users;
+    }
+    public List<User> DtoToEntity(List<User_Dto> dtos)
+    {
+        return dtos.stream().map(x->DtoToEntity(x)).collect(Collectors.toList());
+    }
+
+
+}
